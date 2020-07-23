17cd7c000a6b5fa021048ec2d31bac406322f5d6
Guyoguyes <g.abduba43@gmail.com>
2020-07-24 00:25:50 +0300

    create login and signup pages

--- QuizApp/src/app/app-routing.module.ts
+++ QuizApp/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {HomePageComponent} from "./components/home-page/home-page.component";
 
 
-const routes: Routes = [];
+let routes: Routes;
+routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'homepage', component: HomePageComponent},
+  {path: 'login-page', component: LoginComponent},
+  {path: 'signup-page', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- QuizApp/src/app/app.module.ts
+++ QuizApp/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
