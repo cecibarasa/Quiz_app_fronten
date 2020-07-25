d9ed35b3798f796845f0944523d0485e40bb01a8
Guyoguyes <g.abduba43@gmail.com>
2020-07-25 20:20:20 +0300

    added user login and authentication services method

--- QuizApp/src/app/service/authenication/authentication.service.ts
+++ QuizApp/src/app/service/authenication/authentication.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {environment} from '../../../environments/environment';
+import { User } from 'src/app/models/user/user';
+import { Login } from 'src/app/models/user/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
+
+  noAuthHeader = { headers: new HttpHeaders({ 'noAuth': 'True'})}
+
+  //Register Client
+  addUser(user:User){
+    return this.http.post(environment.authentcationApi + 'signup', user, this.noAuthHeader);
+  }
+
+  //User authentication
+  authenticateUser(login: Login){
+    return this.http.post(environment.authentcationApi + 'login', login, this.noAuthHeader)
+  }
 }
