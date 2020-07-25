ca234715539bbadacfe0c934585458160248b4a1
Guyoguyes <g.abduba43@gmail.com>
2020-07-25 22:23:09 +0300

    authentication interceptor added

--- QuizApp/src/app/app.module.ts
+++ QuizApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +9,8 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { AuthenticationService } from './service/authenication/authentication.service';
+import
 
 @NgModule({
   declarations: [
@@ -25,7 +26,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [AuthenticationService, {
+    provide:HTTP_INTERCEPTORS,
+    useClass: Auth,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
